88ed48847a89544a27ad5d014fc33a112afa0149
Alex Rutherford <alexisadams@gmail.com>
2026-04-03 22:32:50 +0000

    Added LLM call

--- app.py
+++ app.py
@@ -2,14 +2,23 @@ import gradio as gr
 import os
 import openai
 
+from openai import OpenAI
+
+client = OpenAI()
+
+
 def greet(name, intensity):
     return "Hello, " + name + "!" * int(intensity)
 
-def llm_wrapper(name):
-    return None
+def llm_wrapper(name,intensity):
+
+    response = client.responses.create(
+            model="gpt-4.1-nano",
+            input='Say hello to ' + name + 'in Russian')
+    return response.output[0].content[0].text
 
 demo = gr.Interface(
-    fn=greet,
+    fn=llm_wrapper,
     inputs=["text", "slider"],
     outputs=["text"],
     api_name="predict"
