73cfe949e55e2f0016719a5bd966e7254fa82d4e
Alex Rutherford <alexisadams@gmail.com>
2026-04-07 08:42:48 +0000

    Added chat function

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 '''Gradio demo page to expose LLM functionality'''
+import json
 import gradio as gr
 from openai import OpenAI
 
@@ -16,11 +17,24 @@ def llm_wrapper(name):
             input='Say hello to ' + name + 'in Russian')
     return response.output[0].content[0].text
 
+def chat_reponse(message, history):
+    '''Simple catbot response function'''
+    response = client.responses.create(
+            model="gpt-4.1-nano",
+            input = message)
+    print(history)
+    #print(json.dumps(json.loads(history), indent=4))
+    return response.output[0].content[0].text
+
 demo = gr.Interface(
     fn=llm_wrapper,
     inputs=["text"],
     outputs=["text"],
     api_name="predict"
 )
+
+demo = gr.ChatInterface(
+    fn=chat_reponse, 
+).launch()
 
 demo.launch()
